03a4c22347185b562201f2e55608468499459050
ronsavage <ron@savage.net.au>
2013-05-20 09:55:21 +1000

    Clean up script which generates the demo page

--- scripts/generate.demo.sh
+++ scripts/generate.demo.sh
@@ -3,8 +3,11 @@
 perl -Ilib scripts/generate.demo.pl
 perl -Ilib scripts/code.attributes2html.pl
 
-# $DR is my web server's doc root and $PM is Perl-modules/html/graphviz2.pathutils.
+# $DR is my web server's doc root.
+
+PM=Perl-modules/html/graphviz2.pathutils
 
 cp html/*.html html/*.svg $DR/$PM
 
-echo Check the version number in the demo index
+echo Copied files to $DR/$PM
+echo Warning: Check the version number in the demo index
