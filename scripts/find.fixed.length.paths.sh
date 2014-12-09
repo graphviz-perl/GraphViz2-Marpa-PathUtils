c2095ffd911dce92647ac15a86b6e415ca11a4ac
ronsavage <ron@savage.net.au>
2014-12-09 17:54:00 +1100

    Fist successful runs of fixed length paths (01, 03, not 02)

--- scripts/find.fixed.length.paths.sh
+++ scripts/find.fixed.length.paths.sh
@@ -15,3 +15,8 @@ NODE=$2
 perl -Ilib scripts/find.fixed.length.paths.pl -input data/fixed.paths.in.$FILE.gv \
 	-allow_cycles 0 -path_length 1 -start_node $NODE -max info  \
 	-report_paths 1 -output_dot_file out/fixed.paths.out.$FILE.gv
+
+dot -Tsvg data/fixed.paths.in.$FILE.gv > html/fixed.paths.in.$FILE.svg
+dot -Tsvg out/fixed.paths.out.$FILE.gv > html/fixed.paths.out.$FILE.svg
+
+cp html/fixed.paths.*.$FILE.svg $DR/Perl-modules/html/graphviz2.marpa.pathutils
